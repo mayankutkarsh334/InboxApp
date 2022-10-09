86e076a188c9a63ce703b4aaf57094d72cfc9399
mayank334 <mayankutkarsh334@gmail.com>
2022-10-09 17:18:33 +0530

    oauth

--- src/main/java/io/javabrains/InboxApp.java
+++ src/main/java/io/javabrains/InboxApp.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringGitHubLoginApplication {
+public class InboxApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGitHubLoginApplication.class, args);
+		SpringApplication.run(InboxApp.class, args);
 	}
 
 	@RequestMapping("/user")
 	public String user(@AuthenticationPrincipal OAuth2User principal) {
 		System.out.println(principal);
-		return principal.getAttribute("name");
+		return principal.getAttribute("login");
 	}
 }
